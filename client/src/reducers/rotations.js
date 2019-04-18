8399022fc9f27f78af32d34c6053258787dbcc3b
Barry Ross <barry@4d-media.ca>
2019-04-18 12:50:23 -0700

    cleans up reducers

--- client/src/reducers/rotations.js
+++ client/src/reducers/rotations.js
@@ -1,10 +1,10 @@
 import moment from 'moment'
 import { GET_ROTATIONS_SUCCESS } from '../actionTypes'
 
-export default (state = {all:[]}, action) => {
+export default (state = [], action) => {
   switch (action.type) {
 	 case GET_ROTATIONS_SUCCESS:
-	 return { ...state, all:action.data }
+	 return [...action.data]
    default:
     return state
    }
--- client/src/reducers/spots.js
+++ client/src/reducers/spots.js
@@ -1,9 +1,9 @@
 import { GET_SPOTS_SUCCESS } from '../actionTypes'
 
-export default (state = {all:[]}, action) => {
+export default (state = [], action) => {
   switch (action.type) {
 	 case GET_SPOTS_SUCCESS:
-		return { ...state, all:action.data }
+		return [...action.data]
 
    default:
     return state
