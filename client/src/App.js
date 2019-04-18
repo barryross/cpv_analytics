64a07a98896928af72a8ec53feac04210cc80574
Barry Ross <barry@4d-media.ca>
2019-04-18 11:37:29 -0700

    tweaks App and uses boiler ConfigureStore

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import CostPerView from './containers/CostPerView'
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
 
 class App extends Component {
+
+	componentDidMount = () => {
+
+	}
   render() {
     return (
       <div className="App">
-				
+				<CostPerView/>
       </div>
     );
   }
 }
+
+
 
 export default App;
