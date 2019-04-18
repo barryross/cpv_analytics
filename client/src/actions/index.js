48f1d0c56b8235fffc234b3d778ef3c3534dfe63
Barry Ross <barry@4d-media.ca>
2019-04-18 11:38:06 -0700

    adds action creators and constants

---
+++ client/src/actions/index.js
@@ -0,0 +1,14 @@
+
+import { GET_SPOTS, GET_ROTATIONS } from '../actionTypes'
+
+export const getSpots = () =>{
+	return {
+		type: GET_SPOTS
+ }
+}
+
+export const getRotations = () =>{
+	return {
+		type: GET_ROTATIONS
+ }
+}
