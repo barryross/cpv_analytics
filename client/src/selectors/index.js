895d85fc10bd9b8e3b8f1bf51058c9cfa7f314ca
Barry Ross <barry@4d-media.ca>
2019-04-18 14:46:36 -0700

    adds additional selector

--- client/src/selectors/index.js
+++ client/src/selectors/index.js
@@ -20,3 +20,18 @@ export const combinedRotationsAndSpots = createSelector(
 		})
   }
 )
+
+export const combinedRotationsAndSpotsReduced = createSelector(
+  [ getRotations, getSpots ],
+  (rotations, spots) => {
+		return spots.map(spot => {
+			let rotation = rotations.filter(rotation => {
+				return moment(spot.Time, format).isBetween(moment(rotation.Start, format), moment(rotation.End, format))
+			})
+			console.log("rotation found", rotation)
+			spot.Rotation = rotation[0] ? rotation[0].Name : null
+			spot.CPV = (spot.Spend/spot.Views).toFixed(2)	
+			return spot
+		})
+  }
+)
